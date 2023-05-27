6d4ca6fd70018d3a29a460298561801ee144a9e8
mulyant20 <mulyan.t20@gmail.com>
2023-05-27 23:14:07 +0700

    update and delete job, custom error message

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,12 +1,25 @@
+import { GraphQLError } from 'graphql'
 import { getCompany } from './db/companies.js'
-import { createJob } from './db/jobs.js'
+import { createJob, deleteJob, updateJob } from './db/jobs.js'
 import { getJobs, getJob, getJobsByCompany } from './db/jobs.js'
 
 export const resolvers = {
   Query: {
-    job: (__root, { id }) => getJob(id),
+    job: async (__root, { id }) => {
+      const job = await getJob(id)
+      if(!job) {
+        throw notFoundError(`company not found ${id}`)
+      }
+      return job
+    },
     jobs: () => getJobs(),
-    company: (__root, { id }) => getCompany(id),
+    company: async (__root, { id }) => {
+      const company = await getCompany(id)
+      if (!company) {
+        throw notFoundError(`company not found ${id}`)
+      }
+      return company
+    },
   },
 
   Mutation: {
@@ -14,6 +27,11 @@ export const resolvers = {
       const { title, description } = args.input
       const companyId = 'FjcJCHJALA4i'
       return createJob({ companyId, title, description })
+    },
+    deleteJob: (__root, { id }) => deleteJob(id),
+    updateJob: (__root, args) => {
+      const { id, title, description } = args.input
+      return updateJob({ id, title, description })
     },
   },
 
@@ -25,4 +43,10 @@ export const resolvers = {
   Company: {
     jobs: (company) => getJobsByCompany(company.id),
   },
+}
+
+function notFoundError(message) {
+  return new GraphQLError(message, {
+    extensions: { code: 'NOT_FOUND' },
+  })
 }
