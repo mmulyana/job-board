623e026bc3ba0bfea32a4f78d435de639d019d0d
mulyant20 <mulyan.t20@gmail.com>
2023-05-27 22:43:15 +0700

    mutation create new job and input types

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,4 +1,5 @@
 import { getCompany } from './db/companies.js'
+import { createJob } from './db/jobs.js'
 import { getJobs, getJob, getJobsByCompany } from './db/jobs.js'
 
 export const resolvers = {
@@ -8,12 +9,20 @@ export const resolvers = {
     company: (__root, { id }) => getCompany(id),
   },
 
+  Mutation: {
+    createNewJob: (_root, args) => {
+      const { title, description } = args.input
+      const companyId = 'FjcJCHJALA4i'
+      return createJob({ companyId, title, description })
+    },
+  },
+
   Job: {
     company: (job) => getCompany(job.companyId),
     date: (job) => job.createdAt.slice(0, 'yyyy-mm-dd'.length),
   },
 
   Company: {
-    jobs: (company) => getJobsByCompany(company.id)
-  }
+    jobs: (company) => getJobsByCompany(company.id),
+  },
 }
